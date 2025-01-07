c58ecb0d6a4ecf344e5bfd9f0370291132c037ea
amal <amalkanhangad333@gmail.com>
2025-01-08 00:37:14 +0530

    auth session

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,20 +1,20 @@
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
-import { auth } from '@/auth'
-import { signOut } from 'next-auth/react'
+import { auth, signIn, signOut } from '@/auth'
 
 const Navbar = async () => {
-    const session = await auth()
+    const session = await auth();
+
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
-      <nav className="flex justify-between tems center"></nav>
+      <nav className="flex justify-between items-center">
       <Link href="/">
-        <Image src= "/startify_logo.png" alt="logo" width={144} height={30}/>
+        <Image src= "/startify_logo.png" alt="logo" width={144} height={20}/>
       </Link>
 
-      <div className="flex items-center gap-5">
-        {session && session.user?(
+      <div className="flex items-center gap-5 text-black">
+        {session && session?.user ? (
             <>
             <Link href="/startup/create">
             <span>Create</span>
@@ -25,9 +25,15 @@ const Navbar = async () => {
 
             <Link href ={`/user/${session?.id}`}>
             <span>{session?.user?.name}</span>
+            </Link>
             </>
+        ) : (
+            <button onClick={signIn(provider:'github')}>
+            <span>Login</span>
+            </button>
         )}
       </div>
+      </nav>
     </header>
   )
 }
