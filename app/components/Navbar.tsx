ed0dbbe0007ace399ba8eef9409cb2d8b8c90231
amal <amalkanhangad333@gmail.com>
2025-01-07 22:35:47 +0530

    layout update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
+import { auth } from '@/auth'
 
-const Navbar = () => {
+const Navbar = async () => {
+    const session = await auth()
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between tems center"></nav>
+      <Link href="/">
+        <Image src= "/startify_logo.png" alt="logo" width={144} height={30}/>
+      </Link>
     </header>
   )
 }
