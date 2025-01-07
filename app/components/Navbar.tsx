290c28be642de734748f9283767eddf437925cb3
amal <amalkanhangad333@gmail.com>
2025-01-07 22:59:09 +0530

    auth session

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
 import { auth } from '@/auth'
+import { signOut } from 'next-auth/react'
 
 const Navbar = async () => {
     const session = await auth()
@@ -11,6 +12,22 @@ const Navbar = async () => {
       <Link href="/">
         <Image src= "/startify_logo.png" alt="logo" width={144} height={30}/>
       </Link>
+
+      <div className="flex items-center gap-5">
+        {session && session.user?(
+            <>
+            <Link href="/startup/create">
+            <span>Create</span>
+            </Link>
+            <button onClick={signOut}>
+                <span>Logout</span>
+            </button>
+
+            <Link href ={`/user/${session?.id}`}>
+            <span>{session?.user?.name}</span>
+            </>
+        )}
+      </div>
     </header>
   )
 }
