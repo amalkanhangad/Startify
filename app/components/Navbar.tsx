9ff6d8aba2f9d9d0975e38d5a1f170fb97c4eb66
amal <amalkanhangad333@gmail.com>
2025-01-11 20:32:40 +0530

    session rendering

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,36 +3,55 @@ import React from 'react'
 import Image from 'next/image'
 import { auth, signIn, signOut } from '@/auth'
 
+interface Session {
+  user: {
+    name: string;
+    id: string;
+  } | null;
+}
+
 const Navbar = async () => {
-    const session = await auth();
+  const session = await auth() as Session;
+
+  const handleSignIn = async () => {
+    'use server'
+    await signIn('github')
+  }
+
+  const handleSignOut = async () => {
+    'use server'
+    await signOut()
+  }
 
   return (
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
-      <Link href="/">
-        <Image src= "/startify_logo.png" alt="logo" width={144} height={20}/>
-      </Link>
-
-      <div className="flex items-center gap-5 text-black">
-        {session && session?.user ? (
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={104} height={10} />
+        </Link>
+        <div className="flex items-center gap-5 text-black">
+          {session?.user ? (
             <>
-            <Link href="/startup/create">
-            <span>Create</span>
-            </Link>
-            <button onClick={signOut}>
-                <span>Logout</span>
-            </button>
-
-            <Link href ={`/user/${session?.id}`}>
-            <span>{session?.user?.name}</span>
-            </Link>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <form action={handleSignOut}>
+                <button type="submit">
+                  <span>Logout</span>
+                </button>
+              </form>
+              <Link href={`/user/${session.user.id}`}>
+                <span>{session.user.name}</span>
+              </Link>
             </>
-        ) : (
-            <button onClick={signIn(provider:'github')}>
-            <span>Login</span>
-            </button>
-        )}
-      </div>
+          ) : (
+            <form action={handleSignIn}>
+              <button type="submit">
+                <span>Login</span>
+              </button>
+            </form>
+          )}
+        </div>
       </nav>
     </header>
   )
