f707cf17998df6e0c0838ec5601814d9c3784234
amal <amalkanhangad333@gmail.com>
2025-01-13 16:04:15 +0530

    updated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <>
-    <h1 className="text-2xl">Home</h1>
+    <section className="pink_container">
+    <h1 className="heading">Pithch your startup<br/>onnect with entrepreneurs</h1>
+    <p className="sub-heading !max-w-3xl">
+      submit ideas, vote on pitches, and get noticed in virtual competitions
+    </p>
+    </section>
     </>
   );
 }
